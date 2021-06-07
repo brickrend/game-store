fcddce694052daa32d96b54ff92e414c27cfa711
brickrend <Brickrends9@gmail.com>
2021-06-07 19:49:06 +0300

    changed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
-
+import './products'
 import './App.css';
+import games from './products';
+
+const gamesList = games.map((game) => 
+  <div className ="game-margin" key= {game.id}>
+    <img className = "image-List" src = {game.image}/>
+    <p className = "text">{game.name}</p>
+    <p className = "text">{game.price}</p>
+  </div>
+)
 
 function App() {
   return (
@@ -7,6 +16,10 @@ function App() {
       <h1 className="text">Game Store</h1>
       <h2 className="text">Made by gamers for gamers</h2>
       <img id="image-s" src ="https://tessla.org/wp-content/uploads/2017/06/db3c6932ab7f48aa37911fa229efa7ba-2.jpg"/>
+
+      <div className ="list">
+          {gamesList}
+      </div>
     </div>
   );
 }
---
+++ src/products.js
@@ -0,0 +1,17 @@
+const games = [
+    {
+        id: 1,
+        name: "returnal",
+        price: 20,
+        image: "https://upload.wikimedia.org/wikipedia/en/9/91/Returnal_cover_art.jpg",
+    },
+
+    {
+        id : 2,
+        name: "Skyrim",
+        price: 15,
+        image: "https://www.digiseller.ru/preview/89827/p1_1210701_a1b136da.jpeg",
+    }
+]
+
+export default games
